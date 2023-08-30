587746d809588c3876c2987c87a19f6f53f9ffd5
adamavlas <adam@avlas.cz>
2023-08-31 01:24:11 +0200

    basic GUI layout + some basic functions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,12 @@ namespace ADMP
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TopMenuOpenFile(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Opening file...");
+            TopMenuHandler.
         }
     }
 }
---
+++ code/TopMenuHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace ADMP
+{
+    class TopMenuHandler
+    {
+        public void OpenFile() 
+        {
+            string filePath;
+
+            //using (OpenFileDialog fileDialog = new OpenFileDialog())
+            //{
+            //    fileDialog.InitialDirectory = "c:\\";
+
+            //    //if (fileDialog.ShowDialog() == DialogResultConverter.)
+            //    //{
+                    
+            //    //}
+            //}
+        }
+    }
+}
