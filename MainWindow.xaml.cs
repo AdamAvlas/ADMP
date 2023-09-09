c37bc328d04b0c03e75c63b329bc8904aeb576f0
adamavlas <adam@avlas.cz>
2023-09-09 18:21:31 +0200

    completed progressbar slider drag events

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -39,6 +39,8 @@ namespace ADMP
             progressBarHandler = new ProgressBarHandler(this);
 
             mainMediaPlayer.PositionChanged += new EventHandler<MediaPlayerPositionChangedEventArgs>(ProgressBarSliderUpdate);
+            ProgressBarSlider.DragEnter += new DragEventHandler(ProgressBarSliderDragEnter);
+            ProgressBarSlider.DragLeave += new DragEventHandler(ProgressBarSliderDragLeave);
         }
 
         private void TopMenuOpenFile(object sender, RoutedEventArgs e)
@@ -65,10 +67,32 @@ namespace ADMP
         {
             this.ProgressBar.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new PBUpdate(progressBarHandler.UpdateProgressBar));
         }
+        public void ProgressBarSliderDragEnter(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("slider drag entered");
+            //progressBarHandler.SliderDragEnter();
+        }
+        public void ProgressBarSliderDragLeave(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("slider drag left");
+            //progressBarHandler.SliderDragLeave();
+        }
 
         public void AppQuit(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        private void ProgressBarSliderDragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            progressBarHandler.SliderDragEnter();
+            Debug.WriteLine("thumb drag entered");
+        }
+
+        private void ProgressBarSliderDragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            progressBarHandler.SliderDragLeave();
+            Debug.WriteLine("thumb drag completed");
         }
     }
 }
